42392fe78ba97263688c8d1fec978cda4f396cba
dzhuryn <dzhuryn.volodymyr@gmail.com>
2018-03-08 18:02:54 +0200

    Фикс №2
    Иправлен баг из производительностю и убраны пустие строк при переносе из файла в базу

--- assets/plugins/staticElements/plugin.staticElements.php
+++ assets/plugins/staticElements/plugin.staticElements.php
@@ -39,6 +39,15 @@ if(!empty($_SESSION['mgrShortname']) && $showDebug==1 && $modx->isFrontend() && 
     ');
     }
 }
+global $categoryData;
+$C = $modx->getFullTableName('categories');
+$sql = 'SELECT * FROM ' . $C ;
+$result = $modx->db->query($sql);
+$categoryDateResp = $modx->db->makeArray($result);
+foreach ($categoryDateResp as $el) {
+    $categoryData[$el['category']] = $el;
+}
+
 $expansions = array(
     'chunks'=>'tpl',
     'templates'=>'tpl',
@@ -135,29 +144,20 @@ if(!function_exists('GetListFiles')) {
 if(!function_exists('categoryCheck')) {
     function categoryCheck($category)
     {
-
-
         global $modx;
+        global $categoryData;
         $C = $modx->getFullTableName('categories');
         $cat = $modx->db->escape($category);
-        $sql = 'SELECT count(id) FROM ' . $C . ' where `category`="' . $cat . '"';
-
-        $result = $modx->db->query($sql);
-        $count = $modx->db->getValue($result);
-
-
-        if ($count == 0) {
-            $fields = array('category' => $category);
-            $modx->db->insert($fields, $C);
+        if (empty($categoryData[$category])) {
+            $fields = array('category' => $cat);
+            $newCategoryId = $modx->db->insert($fields, $C);
+            $categoryData[$newCategoryId] = [
+                'id'=>$newCategoryId,
+                'category'=>$category,
+                'rank'=>0,
+            ];
         }
-
-        $sql = 'SELECT * FROM ' . $C . ' where `category`="' . $cat . '"';
-        $result = $modx->db->query($sql);
-        $result = $modx->db->getRow($result);
-
-
-        return $result;
-
+        return $categoryData[$category];
     }
 }
 if(!function_exists('fileNameParse')) {
@@ -507,6 +507,14 @@ if ($eventName == 'OnWebPageInit' || $eventName=='OnManagerPageInit' || $eventNa
                     $elementTable = $fieldNames['tableName'];
                     $code = $modx->db->escape($fileFullParams['body']);
                     $code =  str_replace_once('<?php', '', $code);
+                    
+                    $removeFirst = ['\r','\n'];
+                    foreach ($removeFirst as $char) {
+                        if(substr($code,0,2) == $char){
+                            $code = substr($code,2);
+                        }
+
+                    }
                     $fields = array(
                         $fieldNames['name'] => $modx->db->escape($fileFullParams['head']['name']),
                         $fieldNames['description'] => $modx->db->escape($fileFullParams['head']['description']),
--- install/assets/plugins/StaticElements.tpl
+++ install/assets/plugins/StaticElements.tpl
@@ -7,7 +7,7 @@
  * @category    plugin
  * @version     4.3.7.0
  * @license     http://www.gnu.org/copyleft/gpl.html GNU Public License (GPL)
- * @internal    @properties &elementsPath=папка элементов;string;assets/elements/&configFileName=Название конфиг файла;string;config.php&onlyAdmin=Только для админа;string;1&showDebug=Дебаг;string;1
+ * @internal    @properties &elementsPath=папка элементов;string;assets/elements/&configFileName=Название конфиг файла;string;config.php&onlyAdmin=Только для админа;string;1&showDebug=Дебаг;string;0
  * @internal    @events OnManagerPageInit,OnPageNotFound,OnWebPageInit,OnChunkFormDelete,OnChunkFormSave,OnPluginFormDelete,OnPluginFormSave,OnSnipFormDelete,OnSnipFormSave,OnTempFormDelete,OnTempFormSave
  * @internal    @modx_category system
  * @internal    @legacy_names StaticElements
@@ -21,6 +21,6 @@
 //&elementsPath=папка элементов;string;assets/elements/
 //&configFileName=Название конфиг файла;string;config.php
 //&onlyAdmin=Только для админа;string;1
-//&showDebug=Дебаг;string;1
+//&showDebug=Дебаг;string;0
 
 require MODX_BASE_PATH.'assets/plugins/staticElements/plugin.staticElements.php';
